6fe67aa5a65aeaac5cb5d67a8da478320bced577
votyn <votyn@localhost.localdomain>
2018-01-28 03:36:32 +0000

    Embeddified the logging and also fixed "1 minute"s""

--- cmds/mute.js
+++ cmds/mute.js
@@ -1,4 +1,5 @@
 const fs = module.require("fs");
+const Discord = module.require('discord.js');
 const config = require("../config.json");
 
 module.exports.run = async (client, message, args) => {
@@ -14,6 +15,10 @@ module.exports.run = async (client, message, args) => {
     } //Moderators cannot mute other moderators.
 
     let role = message.guild.roles.find(r => r.name === "Muted"); //search for role
+    if (message.guild.me.highestRole.comparePositionTo(role) < 1) {
+        console.log(`ERROR: Cannot assign Muted role!`);
+        logChannel.send(`ERROR: Cannot assign Muted role!`);
+    }
     if (!role) { //if no role, create role
         try {
             role = await message.guild.createRole({ //create role
@@ -50,15 +55,29 @@ module.exports.run = async (client, message, args) => {
         console.log(`args[1]`);
         fs.writeFile("./mutes.json", JSON.stringify(mutes, null, 4), err => {
             if (err) throw err;
-            logChannel.send(`${target.user.username} has been muted for ${args[1]} minutes.`);
-            console.log(`${target.user.username} has been muted for ${args[1]} minutes.`);
         });
-        (await message.channel.send(`${target.user.username} has been muted for ${args[1]} minutes.`)).delete(20000);
+        let s = 's'
+        if (args[1] = 1) { s = '' }
+        logChannel.send({
+            embed: new Discord.RichEmbed()
+                .setDescription(`**Target:** ${target}\n**Moderator:** ${message.author}\n**Time:** ${args[1]} minute${s}.`)
+                .setFooter(`ID: ${target.id}`)
+                .setAuthor(`Member muted!`, target.user.displayAvatarURL)
+                .setTimestamp()
+        });
+        console.log(`${target.user.username} has been muted for ${args[1]} minute${s}.`);
+        (await message.channel.send(`${target.user.username} has been muted for ${args[1]} minute${s}.`)).delete(20000);
     }
 
     if (!args[1]) {
         (await message.channel.send(`${target.user.username} has been muted.`)).delete(20000);
-        logChannel.send(`${target.user.username} has been muted.`);
+        logChannel.send({
+            embed: new Discord.RichEmbed()
+                .setDescription(`**Target:** ${target}\n**Moderator:** ${message.author}`)
+                .setFooter(`ID: ${target.id}`)
+                .setAuthor(`Member muted!`, target.user.displayAvatarURL)
+                .setTimestamp()
+        });
         console.log(`${target.user.username} has been muted.`);
     }
 
