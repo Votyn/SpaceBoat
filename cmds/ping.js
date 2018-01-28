6b39604f2f2551acb81bba28828e3698e29232f7
votyn <votyn@localhost.localdomain>
2018-01-28 22:30:47 +0000

    only bot owner can use this command.

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,4 +1,5 @@
 module.exports.run = async (bot, msg) => {
+    if (!(message.author.id == 166125035092836352)) return;
     const m = await msg.channel.send('Pong!');
     m.edit(`:ping_pong: Pong! \`${m.createdTimestamp - msg.createdTimestamp}ms\``);
 }; //stolen from https://github.com/RayzrDev/RayzrDevBot.git
