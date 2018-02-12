8f0c70a139c12914ca1cbaea65f62d1ad4986352
Votyn <23479402+Votyn@users.noreply.github.com>
2018-02-12 19:17:54 +0000

    Take take take take

---
+++ cmds/eval.js
@@ -0,0 +1,37 @@
+const inspect = require('util').inspect;
+const Discord = require('discord.js');
+
+const clean = input => {
+    const output = typeof input === 'string' ? input : inspect(input);
+    return output.replace(/(@|`)/g, '$1\u200b');
+};
+
+module.exports.run = async (bot, message, args) => {
+    //only I can use this command!
+    if (message.author.id != 166125035092836352) return;
+
+    const input = args.join(' ');
+    if (!input) {
+        (await message.channel.send('You must provide some code to evaluate!')).delete(10000);
+    }
+
+    message.delete(10000);
+
+    try {
+        const output = clean(eval(input));
+        message.channel.send({
+            embed: new Discord.RichEmbed()
+                .addField('Input', `\`\`\`javascript\n${input.substr(0, 256)}\n\`\`\``)
+                .addField('Output', `\`\`\`javascript\n${output.substr(0, 768)}\n\`\`\``)
+                .setFooter(`Requested by ${message.author.tag}`)
+        }).then(m => m.delete(15000));
+    } catch (err) {
+        message.channel.send(`:x: An error has occurred: \`\`\`\n${err.toString().substr(0, 1500)}\n\`\`\``);
+    }
+};
+
+module.exports.help = {
+    name: 'eval',
+    usage: 'eval <js code>',
+    description: 'Evaluates some JavaScript code',
+};
