5f25846d9e1a61891d4fb9f26867fc8bd00eda78
Votyn <votyn@localhost.localdomain>
2018-02-17 23:54:49 +0000

    did some stuff to this one

--- cmds/send.js
+++ cmds/send.js
@@ -1,7 +1,7 @@
 module.exports.run = async (client, message, args) => {
     if (message.author.id == 166125035092836352) {
         message.channel.send(args.splice(0).join(' '))
-        message.delete(0)
+        message.delete(0).catch(err => {console.log(`Couldn't delete message`)})
     }
 }
 module.exports.help = {
