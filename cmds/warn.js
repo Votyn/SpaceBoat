2b89024287bd0e72fb0d0bd815a903926f096835
Votyn <votyn@localhost.localdomain>
2018-02-23 23:52:49 +0000

    Warn command created!

---
+++ cmds/warn.js
@@ -0,0 +1,109 @@
+const Discord = require('discord.js');
+const guilds = require("../configs/guilds.json");
+
+module.exports.run = async (bot, message, args) => {
+    console.log("warning...");
+    // load logChannel
+    const logChannel = message.guild.channels.get(guilds[message.guild.id].logChannelID);
+    // can this user ban members?
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return console.log(`${message.author.username} attempted to warn without sufficient permissions!`);
+    // Get the mentioned member object
+    let target = message.mentions.members.first() || message.guild.members.get(args[0]);
+    // No member specified?
+    if (!target) {
+        console.log(`${message.author.username} failed to specify a target user!`);
+        (await message.channel.send(`Please specify a target user.`)).delete(5000);
+        return;
+    }
+    // no warning string specified?
+    if (!args[1]) {
+        console.log(`${message.author.username} failed to specify a warning string!`);
+        // Random error messages. This is gunna be great.
+        let nowarnerr = bot.utils.randomSelection([
+            `What.. what did they do? I can't just figure that out by myself you know...`,
+            `Please specify a warning.`,
+            `Hey I'm a busy bot, you ask me to add a warn and not tell me why?`,
+            `What did they do?`,
+            `And why am I supposed to warn them?`,
+            `There is no warning message.. What am I supposed to do with this?`,
+            `Please tell me the warning message...`,
+            `Please specify a warning message...`,
+            `You forgot to specify a warning message.`
+        ]);
+        (await message.channel.send(nowarnerr)).delete(10000);
+        return;
+    }
+
+    // BEGIN
+
+    if (isNaN(args[1])) {
+        var severity = 1
+        var warningString = args.splice(1).join(' ')
+    }
+    else if (!args[2]) {
+        console.log(`${message.author.username} failed to specify a warning.`);
+        let nowarnerr = bot.utils.randomSelection([
+            `What did they do? I can't just figure that out by myself you know...`,
+            `Please specify a warning.`,
+            `Hey I'm a busy bot, you ask me to add a warn and not tell me why?`,
+            `What did they do?`,
+            `And why am I supposed to warn them?`,
+            `There is no warning message.. What am I supposed to do with this?`,
+            `Please tell me the warning message...`,
+            `Please specify a warning message...`,
+            `You forgot to specify a warning message.`,
+            `What am I meant to do with just a number?`
+        ])
+        (await message.channel.send(nowarnerr)).delete(10000);
+        return;
+    }
+    else if (args[1] < 0) {
+        console.log(`Invalid severity`)
+        let invalidseverityerror = bot.utils.randomSelection([
+            `That's a bit... not right... Can you supply a *valid* severity value please? Like... from 1 to 10... thanks.`,
+            `That's an invalid severity right there! 1 to 10 please. Only. kthxbai`,
+            `You do realise that the first parameter of this is meant to be a severity value right? like from 1 to 10.. not ${args[1]}`,
+            `Can you keep it to a reasonable number please? Like from 1 to 10? Thank you...`,
+            `How am I meant to work with a negative number for the severity?`,
+            `Pretty sure that's a negative number you just set the severity at. I don't think that works... :joy:`,
+            `That's meant to be a severity value right?`                
+        ]);
+        (await message.channel.send(invalidseverityerror)).delete(10000);
+        return;
+    }
+    else if (args[1] > 10) {
+        console.log(`Invalid severity`)
+        let invalidseverityerror = bot.utils.randomSelection([
+            `That's a bit... not right... Can you supply a *valid* severity value please? Like... from 1 to 10... thanks.`,
+            `That's an invalid severity right there! 1 to 10 please. Only. kthxbai`,
+            `You do realise that the first parameter of this is meant to be a severity value right? like from 1 to 10.. not ${args[1]}`,
+            `Can you keep it to a reasonable number please? Like from 1 to 10? Thank you...`,
+            `That's meant to be a severity value right?`,
+            `That's a bit too big for a severity value.. Just a bit.`,
+            `... That's a bit too big for me... That severity value.. Mind making it smaller? Please? Thanks... :sweat_smile:`
+        ]);
+        (await message.channel.send(invalidseverityerror)).delete(10000);
+        return;
+    };
+    if (!isNaN(args[1])) {
+        var severity = args[1];
+        var warningString = args.splice(2).join(' ')
+    };
+    let userID = target.id
+    let moderator = message.author
+    let guildID = message.guild.id
+
+    await target.send(`**You have received a warning!**\n${warningString}`)
+        .catch(console.error);
+    await bot.utils.warning(bot, guildID, userID, moderator.id, warningString, severity)
+        .then(message.channel.send(`User has been warned.`))
+        .catch(console.error);
+    bot.utils.logChannel(bot, guildID, `Member warned!`, target, moderator, '', '', `\n**Severity:** ${severity}\n**Warning:** ${warningString}`);
+}
+
+module.exports.help = {
+    name: "warn",
+    usage: "warn <user> <severity> <warning>",
+    description: "Adds a warning attached to the user to the database."
+}
+
--- utils.js
+++ utils.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const guilds = require("./configs/guilds.json");
 const sqlite3 = require('sqlite3').verbose();
 
-exports.logChannel = (bot, guildID, event, user, moderator, reason, timeString) => {
+exports.logChannel = (bot, guildID, event, user, moderator, reason, timeString, other, thumbnail) => {
     if (!guildID) return console.log(`guildID is not defined!`);
     // identify the logger channel
     let logChannel = (bot.guilds.get(guildID)).channels.get(guilds[guildID].logChannelID)
@@ -15,14 +15,17 @@ exports.logChannel = (bot, guildID, event, user, moderator, reason, timeString) 
     if (!reason || reason == '') { var reasonString = '' }
     // check for timeString
     if (!timeString || timeString == '') { var timeString = '' }
-    //do the actual embed
+    // check if other supplied
+    if (!other || other == '') { var other = '' } 
+    // do the actual embed
     try {
         logChannel.send({
             embed: new Discord.RichEmbed()
-                .setDescription(`**Target:** ${user} (${user.tag})${moderatorString}${reasonString}${timeString}`)
+                .setDescription(`**Target:** ${user} (${user.tag})${moderatorString}${reasonString}${timeString}${other}`)
                 .setFooter(`ID: ${user.id}`)
                 .setAuthor(event, user.displayAvatarURL)
                 .setTimestamp()
+                .setThumbnail(thumbnail)
         })
     }
     catch (error) {
@@ -32,11 +35,32 @@ exports.logChannel = (bot, guildID, event, user, moderator, reason, timeString) 
     }
 }
 
-exports.warning = (bot, guildID, user, string, severity) => {
-    let db = new sqlite3.Database('./db/warnings', (err) => {
-        if (err) {
-        console.error(err.message);
-        }
-        console.log('Connected to the chinook database.');
+// stolen from RayzrDev SharpBot
+exports.randomSelection = (choices) => {
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+exports.warning = async (bot, guildID, userID, moderatorID, string, severity) => {
+    // open database connection
+    let db = await new sqlite3.Database('./db/users.db', sqlite3.OPEN_CREATE | sqlite3.OPEN_READWRITE, (err) => {
+        if (err) return console.error(err.message);
+        console.log('Connected to the SQLite users database.');
+    })
+    // create warnings table if it doesn't exist
+    console.log(`Creating table...`)
+    await db.run('CREATE TABLE IF NOT EXISTS warnings (guild_id integer, user_id integer, moderator_id integer, warn_str text, severity integer, date integer)', function(err) {
+        if (err) return console.error(err.message);
+        else console.log(`Table created successfully.`);
+    })
+    // perform the insertion
+    await db.run(`INSERT INTO warnings (guild_id, user_id, moderator_id, warn_str, severity, date) VALUES(?,?,?,?,?,?)`, [guildID, userID, moderatorID, string, severity, Date.now()], function(err) {
+        if (err) return console.error(err.message);
+        else console.log(`Successfully inserted warning into SQLite table, with rowid ${this.lastID}`);
     });
+    await db.close((err) => {
+        if (err) {
+          return console.error(err.message);
+        }
+        console.log('Closed database connection.');
+      });
 }
