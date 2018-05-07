2662619d23721664a774e641ae709200c4a73a00
Votyn <votyn@localhost.localdomain>
2018-05-07 20:17:56 +0100

    Improv

--- cmds/edits.js
+++ cmds/edits.js
@@ -1,33 +1,29 @@
 const Discord = module.require("discord.js");
 module.exports.run = async (bot, message, args) => {
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return;
-    console.log('checking edits')
     var msg;
     var edits;
     if (args[1]) {
-        console.log('channel id supplied?')
         let msgid = args[1]
         let channelid = args[0]
         let channel = await message.guild.channels.get(channelid)
         if (channel.id == channelid) {
-            console.log('channel id verified')
             msg = await channel.fetchMessage(msgid)
             if (msg.id == msgid) {
-                console.log('msg id verified')
-                edits = msg.edits.join('\n')
+                edits = msg.edits
             } else return;
         } else return;
     } else {
         let msgid = args[0];
         msg = await message.channel.fetchMessage(msgid)
         if (msg.id == msgid) {
-            console.log('msg id verified')
-            edits = msg.edits.join('\n')
+            edits = msg.edits
         } else return;
     }
-    
+    let editarray = []
+    edits.forEach(e => editarray.unshift(` - ${e}`));
     let embed = new Discord.RichEmbed().setAuthor('Message Edits', msg.author.displayAvatarURL)
-                                       .setDescription(edits)
+                                       .setDescription(editarray)
                                        .setTimestamp(msg.editedAt || msg.createdAt)
     message.channel.send(embed);
 }
