32d4cab09a9950505f3e9030f6d81ffecd478e4b
Votyn <23479402+Votyn@users.noreply.github.com>
2018-05-11 19:01:45 +0100

    Newcomer verification buffer tech for space engine server (untested)

--- cmds/test.js
+++ cmds/test.js
@@ -4,13 +4,7 @@ const Discord = require('discord.js')
 const config = require("../data/config.json")
 module.exports.run = async (bot, message, args) => {
     if (!(message.author.id == config.ownerid)) return;
-    let embed = new Discord.RichEmbed().setTitle('DISCORD LINKS')
-                                            .addField('SpaceEngine Official (this)', 'https://discord.gg/spaceengine')
-                                            .addField('SpaceEngine Italian', 'https://discord.gg/t5BJpfv')
-                                            .addField('SpaceEngine French', 'https://discord.gg/DyEs4TD')
-                                            .addField('RU SpaceEngine (русский)', 'https://discord.gg/aEJ42Ej')
-                                            .addField('SpaceEngine Addons', 'https://discord.gg/RpaDp5q')
-    message.channel.send(embed)
+    console.log(message)
 }
 
 module.exports.help = {
